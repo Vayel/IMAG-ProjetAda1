0f75d176a58dc2f5000e31d3ad775f8febb07888
Vincent Lefoulon <vincent.lefoulon@free.fr>
2015-11-09 15:17:10 +0100

    Implement svg.adb

--- svg.adb
+++ svg.adb
@@ -1,16 +1,40 @@
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
+
+
 package body SVG is
-	function Header(w, h: Integer) return String is
+  contents: Unbounded_String := To_Unbounded_String("");
+
+  procedure init is
   begin
-    return "<svg width=""" & Integer'image(w) & """ height=""" & Integer'image(h) & """>";
+    contents := "";
   end;
 
-	function Footer return String is
+  procedure Header(w, h: Integer) is
   begin
-    return "</svg>";
+    contents := contents & "<svg width=""" & Integer'image(w) & """"; 
+    contents := contents & " height=""" & Integer'image(h) & """>";
   end;
 
-	function Polygon(pts: Points, width, color: String) return String is
+  procedure Footer is
   begin
-    null; -- TODO
+    contents := contents & "</svg>";
+  end;
+
+  procedure Polygon(pts: Points; width, color: String) is
+  begin
+    contents :=  contents & "<polygon points=""";
+
+    for k in  pts'range loop
+      contents := contents & Integer'image(pts(k).x) & ",";
+      contents := contents & Integer'image(pts(k).y) & " ";
+    end loop;
+
+    contents := contents & """ style=""stroke-width:" & width & ";";
+    contents := contents &  "stroke:" & color & """ />";
+  end;
+
+  function get_contents return String is
+  begin
+    return to_string(contents);
   end;
 end SVG;
